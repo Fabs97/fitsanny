6f55cfff096b2171fcd870efb785948173313389
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2025-11-09 02:28:21 +0100

    added training to trainings view when filled in

--- lib/pages/training/trainings_view.dart
+++ lib/pages/training/trainings_view.dart
@@ -15,56 +15,69 @@ class TrainingsView extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Center(child: Text('No trainings available, add one now!')),
-                  ElevatedButton(
-                    onPressed: () {
-                      context.read<TrainingBloc>().add(NewTrainingEvent());
-                      context.go('/training/new');
-                    },
-                    child: Text('Add Training'),
-                  ),
+                  _buildNewTrainingButton(context),
                 ],
               )
             : state is TrainingsLoaded
-            ? SingleChildScrollView(
-                child: Column(
-                  children: [
-                    for (var training in state.trainings)
-                      Dismissible(
-                        key: Key(training.id.toString()),
-                        onDismissed: (direction) async {
-                          if (direction == DismissDirection.endToStart) {
-                            // deleted
-                            context.read<TrainingBloc>().add(
-                              RemoveTrainingEvent(training.id!),
-                            );
-                          }
-                        },
-                        background: Container(
-                          color: Colors.red,
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 16.0),
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: Icon(
-                                Icons.delete,
-                                size: 32.0,
-                                color: Colors.white70,
+            ? Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          for (var training in state.trainings)
+                            Dismissible(
+                              key: Key(training.id.toString()),
+                              onDismissed: (direction) async {
+                                if (direction == DismissDirection.endToStart) {
+                                  // deleted
+                                  context.read<TrainingBloc>().add(
+                                    RemoveTrainingEvent(training.id!),
+                                  );
+                                }
+                              },
+                              background: Container(
+                                color: Colors.red,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(right: 16.0),
+                                  child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Icon(
+                                      Icons.delete,
+                                      size: 32.0,
+                                      color: Colors.white70,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              child: ListTile(
+                                title: Text(training.title),
+                                subtitle: Text(
+                                  'Exercises: ${training.exercises.length}',
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        child: ListTile(
-                          title: Text(training.title),
-                          subtitle: Text(
-                            'Exercises: ${training.exercises.length}',
-                          ),
-                        ),
+                        ],
                       ),
-                  ],
-                ),
+                    ),
+                  ),
+                  _buildNewTrainingButton(context),
+                ],
               )
             : Center(child: Text('Loading trainings...'));
       },
+    );
+  }
+
+  _buildNewTrainingButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        context.read<TrainingBloc>().add(NewTrainingEvent());
+        context.go('/training/new');
+      },
+      child: Text('Add Training'),
     );
   }
 }
